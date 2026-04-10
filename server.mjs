3fd00858e61405304a8b1a5faa5ad72da7823579
Sarthak <Sarthakmp007@gmail.com>
2026-04-10 14:08:39 +0530

    Added Reg page

--- server.mjs
+++ server.mjs
@@ -1,10 +1,17 @@
 import express from "express"
-import mongod from "mongod"
+import mongoose from "mongoose";
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 
-app.set("view engine","ejs");
-app.set("views","views");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
@@ -12,13 +19,15 @@ const PORT = 3000;
 
 
 app.get("/",(req,res)=>{
-
-    res.render("index",{data:"Hello"});
-    
+    res.render("index",{title:"home"});
 });
 
-app.get("/register",async (req,res)={
+app.get("/register",async (req,res)=>{
+    res.render("RegistrationPage",{title:"Register"});
+});
 
+app.get("/login",async (req,res)=>{
+    //Render Login Page
 });
 
 app.listen(PORT,()=>{
